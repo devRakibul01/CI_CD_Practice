dbf5131eac09756594aeeb44c5676f8769100f3f
rakibDev <rakibul.codeware@gmail.com>
2026-04-28 16:42:34 +0600

    create project and crate a controller and write a enpoint and print a string

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPIForCI_CD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("hello")]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
